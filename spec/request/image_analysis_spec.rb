76170e26def2f85f38d06aa4bc8076a96bcc5c39
maxarvid <max.arvid.anderson@gmail.com>
2022-01-25 18:33:47 +0100

    adds 1st specs for text analysis

--- spec/request/image_analysis_spec.rb
+++ spec/request/image_analysis_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe Api::AnalysesController, type: :request do
   describe 'Image analysiss' do
     describe 'SAFE' do
+     
     end
 
     describe 'UNSAFE' do
--- spec/request/text_analysis_spec.rb
+++ spec/request/text_analysis_spec.rb
@@ -2,5 +2,21 @@
 
 RSpec.describe Api::AnalysesController, type: :request do
   describe 'Text analysis' do
+    before do
+      post '/api/analyses', params: {
+        analysis: {
+          resource: 'Hi there, did you bring your coat?',
+          category: 'text'
+        }
+      }
+    end
+
+    it 'is expected to return a 200 status code' do
+      expect(response.status).to eq 200
+    end
+
+    it 'is expected to classify the text as clean' do
+      expect(eval(JSON.parse(response.body)['results']['classifications'])[0]['tag_name']).to eq 'clean'
+    end
   end
 end
