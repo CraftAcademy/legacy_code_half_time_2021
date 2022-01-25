cd68cf447065781efe12a9abc52bf0289f25ef3e
shagufta <shagufta.batool8@gmail.com>
2022-01-25 13:14:42 +0100

    text profinity test

--- spec/request/text_analysis_spec.rb
+++ spec/request/text_analysis_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe Api::AnalysesController, type: :request do
        category: :text },
        }
     end
-    it "responds with an text analysis" do
+    it " is expected to respond with status code" do
       expect(response.status).to eq 200
   
+    end
+    it 'is expected to respond with degree of profinity' do
+      #binding.pry
+      expect(eval(JSON.parse(response.body)['results']['classifications'])[0]['tag_name']
+      ).to eq 'clean'
+      
     end
   end
 end
