42829f8b6bea1091e716312ebc2599a27359686a
shagufta <shagufta.batool8@gmail.com>
2022-01-29 10:00:54 +0100

    rectified some error in test files

--- spec/request/text_analysis_spec.rb
+++ spec/request/text_analysis_spec.rb
@@ -1,25 +1,38 @@
 RSpec.describe Api::AnalysesController, type: :request do
   describe "Text analysis" do
     describe 'successfully' do
+      describe 'for single query' do
+        before do
+          post "/api/analyses", params: {
+           analysis: { resource: "This is awesome",
+           category: :text },
+           }
+         end
+         it " is expected to respond with status code" do
+          expect(response.status).to eq 200
       
-    
-    before do
-      post "/api/analyses", params: {
-       analysis: { resource: "This is awesome",
-       category: :text },
-       }
+         end
+         it 'is expected to respond with degree of profinity' do
+          #binding.pry
+          expect(eval(JSON.parse(response.body)['results']['classifications'])[0]['tag_name']
+          ).to eq 'clean'
+          
+          
+        end 
+      end
+      describe 'For several queries' do
+        before do
+          post '/api/analyses', params: {
+            analysis: {
+              resource: ['Hi there, did you bring your coat?', 'Nice weather we are having'],
+              category: 'text'
+            }
+          }
+         end
+         it 'is expected to do analyze array of strings.' do
+        end
+      end
     end
-    it " is expected to respond with status code" do
-      expect(response.status).to eq 200
-  
-    end
-    it 'is expected to respond with degree of profinity' do
-      #binding.pry
-      expect(eval(JSON.parse(response.body)['results']['classifications'])[0]['tag_name']
-      ).to eq 'clean'
-      
-    end
-   end
    describe 'unsuccesfully ' do
      describe 'when there is no paramenter' do
        before do
@@ -35,9 +48,14 @@ RSpec.describe Api::AnalysesController, type: :request do
         
       end
       
-    
-    
       describe 'when the category param is set to image' do
+        before do
+          post '/api/analyses', params: { analysis: { resource: 'This is a string sentence', category: :image } }
+        end
+
+        it 'is expected to return an error message' do
+          expect(JSON.parse(response.body)['results']['error']).to eq '400 Bad Request'
+        end
       end
       
     end
